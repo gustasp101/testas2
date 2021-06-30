fa7242d9c089dac3d7267beb91e1249c53021662
gustas <gpladis@gmail.com>
2021-06-30 19:06:08 +0300

    first commit

---
+++ mypackage/Main.java
@@ -0,0 +1,10 @@
+package com.example.mypackage;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main (String[]args){
+        boolean[] is = new boolean[10];
+        System.out.println(is[5]);
+    }
+}
